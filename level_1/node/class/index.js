7b1e58447d48030fba73d2f86b56e39748679e9d
Christian Oliveira <christian.soliveira@outlook.com>
2020-04-14 16:58:26 -0300

    :tada: create base structure

---
+++ level_1/node/class/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Hello World",
+  });
+});
+
+app.listen(3333, () => {
+  console.log(`🚀 Running at 3333`);
+});
