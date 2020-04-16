81b40bce7d5be0e2804df22d646fb1867582a64a
Christian Oliveira <christian.soliveira@outlook.com>
2020-04-16 17:15:01 -0300

    :heavy_plus_sign: (file-loader) Added webpack loader
    This loader allow import files with extensions like png, jpg, jpeg and gif

--- level_1/react/class/src/App.js
+++ level_1/react/class/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+import "./App.css";
+import backgroundImage from "./assets/unsplash.jpg";
+
 import Header from "./components/Header";
 
 function App() {
@@ -15,7 +18,7 @@ function App() {
   return (
     <>
       <Header title="Projects" />
-
+      <img src={backgroundImage} width={300} />
       <ul>
         {projects.map((project) => (
           <li key={project}>{project}</li>
