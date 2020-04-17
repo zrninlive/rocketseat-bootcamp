69fc87caba80f1252786a9419513dafedc234d9f
Christian Oliveira <christian.soliveira@outlook.com>
2020-04-17 14:40:37 -0300

    :tada: Getting start TypeScript

---
+++ level_1/typescript/src/index.ts
@@ -0,0 +1,11 @@
+import express, { response } from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({
+    message: "hello world",
+  });
+});
+
+app.listen(3333);
