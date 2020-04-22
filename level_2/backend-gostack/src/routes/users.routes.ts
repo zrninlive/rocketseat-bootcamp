088712229fe58e83fa7068fe8ad7924f266cc717
Christian Oliveira <christian.soliveira@outlook.com>
2020-04-22 17:43:49 -0300

    :sparkles: Upload image user avatar
    Uploading images using library multer

--- level_2/backend-gostack/src/routes/users.routes.ts
+++ level_2/backend-gostack/src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router, Request, Response } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request: Request, response: Response) => {
   try {
@@ -21,5 +27,29 @@ usersRouter.post('/', async (request: Request, response: Response) => {
     });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request: Request, response: Response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatar_filename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+  },
+);
 
 export default usersRouter;
