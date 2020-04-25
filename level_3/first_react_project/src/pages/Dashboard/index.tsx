e6b7801cf946e4e9df8238cb2c6f57444960fd29
Christian Oliveira <christian.soliveira@outlook.com>
2020-04-25 19:16:12 -0300

    :rocket: Create and stylizing repository page

--- level_3/first_react_project/src/pages/Dashboard/index.tsx
+++ level_3/first_react_project/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import { Title, Form, Repositories, Error } from './styles';
@@ -24,9 +25,9 @@ const Dashboard: React.FC = () => {
 
     if (storagedRepositories) {
       return JSON.parse(storagedRepositories);
-    } else {
-      return [];
     }
+
+    return [];
   });
 
   useEffect(() => {
@@ -78,7 +79,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -88,7 +92,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- level_3/first_react_project/src/pages/Repository/index.tsx
+++ level_3/first_react_project/src/pages/Repository/index.tsx
@@ -1,7 +1,72 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import githubLogo from '../../assets/github_logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={githubLogo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/10995394?s=460&u=fde11355205c04142c93b2c0746cc361597e87b3&v=4"
+            alt="Christian Oliveira"
+          />
+          <div>
+            <strong>Rockeseat/unform</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+              possimus consequuntur commodi mollitia. Necessitatibus voluptas
+              aliquid perferendis. Culpa mollitia quisquam assumenda repudiandae
+              omnis, sint quis deserunt fuga, eum adipisci aut?
+            </p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="">
+          <div>
+            <strong>asdas</strong>
+            <p>asidoas</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
