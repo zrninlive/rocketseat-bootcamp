9f5a93849663afac2d21e68be4f5b52cff411814
Christian Oliveira <christian.soliveira@outlook.com>
2020-05-04 11:43:19 -0300

    :sparkles: Added authContext and implementing SignIn page

--- level_3/gobarber-web/src/App.tsx
+++ level_3/gobarber-web/src/App.tsx
@@ -4,9 +4,13 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
+import { AuthProvider } from './hooks/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
     <GlobalStyle />
   </>
 );
