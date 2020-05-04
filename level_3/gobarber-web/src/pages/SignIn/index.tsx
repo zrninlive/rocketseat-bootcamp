e62abc5ed45fe25fc5e327d7e2c4db82ddb857e4
Christian Oliveira <christian.soliveira@outlook.com>
2020-05-04 14:31:45 -0300

    :sparkles: Created toast notifications hooks

--- level_3/gobarber-web/src/pages/SignIn/index.tsx
+++ level_3/gobarber-web/src/pages/SignIn/index.tsx
@@ -4,7 +4,9 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -23,6 +25,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -49,9 +52,15 @@ const SignIn: React.FC = () => {
           const errors = getValidationErrors(err);
           formRef.current?.setErrors(errors);
         }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na autênticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credênciais',
+        });
       }
     },
-    [signIn],
+    [signIn, addToast],
   );
 
   return (
