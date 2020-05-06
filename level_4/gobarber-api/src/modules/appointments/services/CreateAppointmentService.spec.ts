c6eb99cbffccc16d1c0a8f27ac8d54227b329922
Christian Oliveira <christian.soliveira@outlook.com>
2020-05-06 18:29:09 -0300

    :heavy_plus_sign: Added JEST to integratino and unit tests

---
+++ level_4/gobarber-api/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,7 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {});
+
+  //it('should not be able two appointments on the same date', () => {});
+});
