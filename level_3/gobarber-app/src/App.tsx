921ed6a20ce907cb8557f888ca81c009f66575e8
Christian Oliveira <christian.soliveira@outlook.com>
2020-05-09 11:24:55 -0300

    :rocket: Imported react-navigation and create routes

--- level_3/gobarber-app/src/App.tsx
+++ level_3/gobarber-app/src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import 'react-native-gesture-handler';
 
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <View>
-    <Text>Home</Text>
-  </View>
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
 );
 
 export default App;
