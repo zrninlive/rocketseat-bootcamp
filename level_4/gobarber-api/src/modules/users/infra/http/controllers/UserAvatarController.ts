284d6783332302208f65392e628bc90cde73e15d
Christian Oliveira <christian.soliveira@outlook.com>
2020-06-10 23:59:41 -0300

    :heavy_plus_sign: Added class-transformer
    Remove password and set avatar_url on class User

--- level_4/gobarber-api/src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ level_4/gobarber-api/src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
-
+import { classToClass } from 'class-transformer';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 export default class UserAvatarController {
@@ -12,8 +12,6 @@ export default class UserAvatarController {
       avatar_filename: request.file.filename,
     });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
