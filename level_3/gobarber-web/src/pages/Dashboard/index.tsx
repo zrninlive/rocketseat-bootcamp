aa86fda170eeb5ea1b7312fa8dcd3e6970709eb2
Christian Oliveira <christian.soliveira@outlook.com>
2020-06-19 21:23:58 -0300

    :sparkles: Create schedule list dashboard

--- level_3/gobarber-web/src/pages/Dashboard/index.tsx
+++ level_3/gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
 
   return (
@@ -28,6 +40,89 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="http://localhost:3333/files/090bc95ec50150d1ef85-polo.jpeg"
+                alt="Poleto"
+              />
+
+              <strong>Poleto Pinga Pinto</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/090bc95ec50150d1ef85-polo.jpeg"
+                  alt="Poleto"
+                />
+
+                <strong>Poleto Pinga Pinto</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/090bc95ec50150d1ef85-polo.jpeg"
+                  alt="Poleto"
+                />
+
+                <strong>Poleto Pinga Pinto</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                14:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/090bc95ec50150d1ef85-polo.jpeg"
+                  alt="Poleto"
+                />
+
+                <strong>Poleto Pinga Pinto</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
